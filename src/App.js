ebf884aaa3c747c18005ccca64e021fda4e94ca1
jaballogian <jaballogianrma77@gmail.com>
2021-09-08 16:02:14 +0700

    implement qrcode in App component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+
+import QRCode from 'qrcode'
 
 function App() {
+  const [ imageSource, setImageSource ] = useState(null)
+
+  const text = 'hello world!'
+
+  QRCode.toDataURL(text)
+  .then(url => {
+    console.log(url)
+    setImageSource(url)
+  })
+  .catch(err => {
+    console.error(err)
+  })
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div>
+        {imageSource && <img src={imageSource} alt=''/>}
+      </div>
+      {text}
     </div>
-  );
+  )
 }
 
 export default App;
